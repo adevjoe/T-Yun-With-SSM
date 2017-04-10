8d9bb32746780b53077baa34585f9514855d8c02
thet <login@thet.ren>
2017-04-11 00:46:54 +0800

    修复sql脚本不能走动创建数据库

--- src/main/resources/sql/ty_u_disk.sql
+++ src/main/resources/sql/ty_u_disk.sql
@@ -1,8 +1,1 @@
-CREATE TABLE `ty_u_disk` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `mark` varchar(16) DEFAULT NULL,
-  `code` varchar(8) DEFAULT NULL,
-  `url` varchar(255) DEFAULT NULL,
-  `create_time` bigint(14) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8
+CREATE TABLE `ty_u_disk` (  `Id` int(11) NOT NULL AUTO_INCREMENT,  `mark` varchar(16) DEFAULT NULL,  `code` varchar(8) DEFAULT NULL,  `url` varchar(100) DEFAULT NULL,  `create_time` varchar(14) DEFAULT NULL,  PRIMARY KEY (`Id`)) ENGINE=InnoDB DEFAULT CHARSET=utf8;
