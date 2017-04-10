a7377912edc79fb823b0289a8531152b361d3c6b
thet <login@thet.ren>
2017-04-11 00:54:32 +0800

    to LF

--- src/main/resources/sql/ty_u_disk.sql
+++ src/main/resources/sql/ty_u_disk.sql
@@ -1,1 +1,8 @@
-CREATE TABLE `ty_u_disk` (  `Id` int(11) NOT NULL AUTO_INCREMENT,  `mark` varchar(16) DEFAULT NULL,  `code` varchar(8) DEFAULT NULL,  `url` varchar(100) DEFAULT NULL,  `create_time` varchar(14) DEFAULT NULL,  PRIMARY KEY (`Id`)) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `ty_u_disk` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `mark` varchar(16) DEFAULT NULL,
+  `code` varchar(8) DEFAULT NULL,
+  `url` varchar(100) DEFAULT NULL,
+  `create_time` varchar(14) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
